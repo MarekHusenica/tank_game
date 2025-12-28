b420c6144ceb2af49c527fd0400323eda78fa3a4
MarekHusenica <marek.husenica@gmail.com>
2025-12-28 21:58:23 +0100

    Initial commit

---
+++ build/CMakeFiles/TankGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TankGame.dir/src/main.cpp.o"
+  "CMakeFiles/TankGame.dir/src/main.cpp.o.d"
+  "CMakeFiles/TankGame.dir/thirdparty/glad/src/glad.c.o"
+  "CMakeFiles/TankGame.dir/thirdparty/glad/src/glad.c.o.d"
+  "TankGame"
+  "TankGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/TankGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
